0dfd250524f8a3a2c74295acb2a40e5e77f0c7ce
EdCalzar <calzaredrick25@gmail.com>
2025-10-22 19:21:24 +0800

    Login page responsive update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,50 @@
-import NavbarLogin_Signup from "../components/NavbarLogin_Signup"
-import Logo from "../assets/logo.png"
+import NavbarLogin_Signup from "../components/NavbarLogin_Signup";
+import Logo from "../assets/logo.png";
 
 export default function Login() {
-    return (
-        <>
-            <NavbarLogin_Signup />
-            <img src={Logo} alt="logo" className="mt-16 mx-auto w-32" />
-            
-            {/*Login card component*/}
-            <div className="bg-white p-5 text-center w-full max-w-sm mx-auto rounded-lg drop-shadow-lg/25">
-                <h1 className="font-bold text-xl my-8 tracking-wider">LOGIN</h1>
-                <input type="textbox" placeholder="Email Address" className="border rounded-md p-2 text-sm w-75 mb-5"/>
-                <input type="textbox" placeholder="Password" className="border rounded-md p-2 text-sm w-75"/>
-                
-                <a href="#" className="inline-block text-[#01165A] text-sm my-8">Forgot Password?</a>
+  return (
+    <>
+      <NavbarLogin_Signup />
 
-                {/*Signin button*/}
-                <button className="border border-black block bg-[#00BC3A] text-[#FFFFFF] text-sm font-bold p-2 rounded-md w-75 mx-auto cursor-pointer tracking-wider">SIGN IN</button>
+      {/* Center logo */}
+      <img src={Logo} alt="logo" className="mx-auto w-24 sm:w-32 md:w-40" />
+      {/* w-24 = small on phones, bigger on tablet/desktop */}
 
-                <p className="text-sm my-8">Don't have an account? <a href="#" className="text-[#01165A]">Sign up here</a></p>
-            </div> 
-        </>
-    )
+      {/* Login card */}
+      <div className="bg-white p-6 sm:p-8 text-center w-11/12 sm:w-full max-w-sm mx-auto rounded-lg shadow-md">
+        <h1 className="font-bold text-xl sm:text-2xl my-6 tracking-wider">LOGIN</h1>
+
+        <div className="flex flex-col items-center space-y-4">
+          {/* Input fields */}
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="border rounded-md p-2 text-sm w-full max-w-xs focus:outline-none focus:ring-2 focus:ring-[#00BC3A]"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="border rounded-md p-2 text-sm w-full max-w-xs focus:outline-none focus:ring-2 focus:ring-[#00BC3A]"
+          />
+        </div>
+
+        {/* Forgot password link */}
+        <a href="#" className="inline-block text-[#01165A] text-sm my-6 hover:underline">
+          Forgot Password?
+        </a>
+
+        {/* Signin button */}
+        <button className="border border-black bg-[#00BC3A] text-white text-sm font-bold p-2 rounded-md w-full max-w-xs mx-auto hover:bg-[#00a732] transition">
+          SIGN IN
+        </button>
+
+        <p className="text-sm my-6">
+          Don't have an account?{" "}
+          <a href="#" className="text-[#01165A] hover:underline">
+            Sign up here
+          </a>
+        </p>
+      </div>
+    </>
+  );
 }
