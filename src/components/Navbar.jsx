8709945c9322d8e622c898018f294704f424fd23
EdCalzar <calzaredrick25@gmail.com>
2025-10-27 20:38:04 +0800

    Updated Signup and Home Page, made home page responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom"
+
 export default function Navbar() {
     return (
         <>
-            {/*Navbar code here*/}
+            <div className="bg-white h-18 flex justify-end gap-12 items-center">
+                    <Link to="/home">Home</Link>
+                    <Link to="/view_reports">View Report</Link>
+                    <Link to="/report_hazard">Report Hazard</Link>
+                    <Link to="/emergency">Emergency</Link>
+                    <Link to="/" className="mr-12">Log Out</Link>
+            </div>
         </>
     )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,48 @@
+import { Link } from "react-router-dom"
+import NavbarLogin_Signup from "../components/NavbarLogin_Signup"
+
 export default function Signup() {
     return (
         <>
-            {/*Insert code here*/}
-            <h1>Sign up page</h1>
+            <NavbarLogin_Signup/>
+            <div className="bg-white p-6 px-9 md:w-2/7 md:mt-12 mx-auto rounded-lg shadow-md">
+                <h1 className="font-bold text-xl">SIGN UP</h1>
+                <p>Create an account</p>
+                <div className="flex flex-col gap-3 md:mt-5">
+                <input
+                    type="text"
+                    placeholder="First Name"
+                    className="border rounded-md p-2 text-sm w-full px-4 focus:outline-none focus:ring-1 focus:ring-[#2b2b2b]"
+                />
+                <input
+                    type="text"
+                    placeholder="Last Name"
+                    className="border rounded-md p-2 text-sm w-full px-4 focus:outline-none focus:ring-1 focus:ring-[#2b2b2b]"
+                />
+                <input
+                    type="text"
+                    placeholder="Email Address"
+                    className="border rounded-md p-2 text-sm w-full px-4 focus:outline-none focus:ring-1 focus:ring-[#2b2b2b]"
+                />
+                <input
+                    type="text"
+                    placeholder="Contact Number"
+                    className="border rounded-md p-2 text-sm w-full px-4 focus:outline-none focus:ring-1 focus:ring-[#2b2b2b]"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="border rounded-md p-2 text-sm w-full px-4 focus:outline-none focus:ring-1 focus:ring-[#2b2b2b]"
+                />
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    className="border rounded-md p-2 text-sm w-full px-4 focus:outline-none focus:ring-1 focus:ring-[#2b2b2b]"
+                />
+                <button className="cursor-pointer bg-[#00BC3A] text-white rounded-sm py-3 md:mt-3 uppercase text-sm hover:bg-[#008a2c] transition-all duration-300 ease-in-out">Register</button>
+                </div>
+                <p className="text-sm text-center mt-8 mb-2 ">Already have an account? <Link to="/" className="text-[#01165A] hover:underline">Sign in here</Link></p>
+            </div>
         </>
     )
 }
