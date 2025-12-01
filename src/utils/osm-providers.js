6fbed64fa162dd82ed6c44da27d32061d54f45f4
EdCalzar <calzaredrick122503@gmail.com>
2025-12-02 00:31:21 +0800

    map added

---
+++ src/utils/osm-providers.js
@@ -0,0 +1,7 @@
+export default {
+    maptiler: {
+        url: "https://api.maptiler.com/maps/openstreetmap/256/{z}/{x}/{y}.jpg?key=ZrndhvZv9TMB9oOYC1Yn",
+        attribution: '&copy; <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    }
+}
+
